eb4e9eb902e96b464e5bb63ec3a32044c5651b0b
eu-jaque <sdias6565@gmail.com>
2026-03-18 14:49:45 -0300

    mudanca

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useLanguage } from "@/contexts/LanguageContext";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/cyberguard-logo.png";
 
 const Footer = () => {
   const { t } = useLanguage();
