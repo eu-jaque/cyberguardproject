62ce00523de986ad5da94b919d38d5ea5af3055b
eu-jaque <sdias6565@gmail.com>
2026-03-30 14:38:21 -0300

    oi gabi

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -21,7 +21,7 @@ const tabs: { label: string; value: ContentType }[] = [
   { label: "Posts", value: "posts" },
   { label: "Artigos", value: "articles" },
   { label: "Vídeos", value: "videos" },
-  { label: "Notícias", value: "news" },
+  { label: "Notíciass", value: "news" },
   { label: "CyberLab", value: "cyberlab" },
 ];
 
