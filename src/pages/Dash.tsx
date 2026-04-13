f9b907dff284ad715b7f771977745a96fbe263a5
noemisthefanycoelho-bip <noemisthefanycoelho@gmail.com>
2026-04-13 14:24:19 -0300

    oi

--- src/pages/Dash.tsx
+++ src/pages/Dash.tsx
@@ -113,22 +113,23 @@ export default function Dash() {
           </div>
         </header>
 
-        {activeSection === "chatbot" || activeSection === "expert" ? (
-          <div className="flex-1 overflow-hidden">
-            {activeSection === "chatbot" && <ChatBotView />}
-            {activeSection === "expert" && <ExpertProfileEmbed />}
-          </div>
-        ) : (
-          <div className="p-6 md:p-10 flex-1 overflow-y-auto">
-            {activeSection === "overview" && <DashOverview courses={courses} userName={profileName} />}
-            {activeSection === "blog" && <DashBlog />}
-            {activeSection === "courses" && <DashCourses courses={courses} userEmail={user?.email} />}
-            {activeSection === "certificates" && <DashCertificates isComplete={false} progressPct={0} />}
-            {/* {activeSection === "community" && <DashCommunity userEmail={user?.email} />} */}
-            {activeSection === "quiz" && <DashQuiz />}
-            {activeSection === "verifiers" && <DashVerifiers />}
-          </div>
-        )}
+        <div className="p-6 md:p-10">
+          {/* Section Content */}
+          {activeSection === "overview" && <DashOverview courses={courses} />}
+          {activeSection === "blog" && <DashBlog />}
+          {activeSection === "courses" && <DashCourses courses={courses} userEmail={user?.email} />}
+          {activeSection === "certificates" && (
+            <DashCertificates
+              isComplete={courses.length > 0}
+              progressPct={courses.length > 0 ? 100 : 0}
+              studentName={profileName}
+              courseName={courses[0]?.title || "Curso CyberGuard"}
+            />
+          )}
+          {activeSection === "community" && <DashCommunity userEmail={user?.email} />}
+          {activeSection === "quiz" && <DashQuiz />}
+          {activeSection === "verifiers" && <DashVerifiers />}
+        </div>
       </div>
     </div>
   );
